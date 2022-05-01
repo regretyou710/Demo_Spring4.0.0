3a8f92565c89df65d2d9fae1968cd2f94d9d2ac4
eason <regretyou710@gmail.com>
2022-05-01 20:33:20 +0800

    202205012033

---
+++ Demo15_bean的後置處理器/src/tw/com/ioc/life/AfterHandler.java
@@ -0,0 +1,29 @@
+package tw.com.ioc.life;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+// bean後處理器
+public class AfterHandler implements BeanPostProcessor {
+
+	// 初始化前
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("step3. 初始化前");
+
+		Person p = (Person) bean;
+		if (p.getSex().equals("男"))
+			p.setName("張無忌");
+		else
+			p.setName("趙敏");
+		return p;
+	}
+
+	// 初始化後
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("step3. 初始化後");
+		return bean;
+	}
+
+}
